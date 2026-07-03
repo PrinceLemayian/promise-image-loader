80194fb03f5d912a551ca0c5f03692dc4dbea374
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-07-03 03:46:05 +0300

    feat: implement async image loading with async/await and Promise.all

--- script.js
+++ script.js
@@ -26,22 +26,55 @@ const createImage = function (imgPath) {
 
 let currentImg;
 
-createImage("img/img-1.jpg")
-  .then((img) => {
-    currentImg = img;
+// createImage("img/img-1.jpg")
+//   .then((img) => {
+//     currentImg = img;
+//     console.log("Image 1 loaded");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//     return createImage("img/img-2.jpg");
+//   })
+//   .then((img) => {
+//     currentImg = img;
+//     console.log("Image 2 loaded");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//   })
+//   .catch((err) => console.error(err));
+
+const loadNPause = async function () {
+  try {
+    let img = await createImage("img/img-1.jpg");
     console.log("Image 1 loaded");
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = "none";
-    return createImage("img/img-2.jpg");
-  })
-  .then((img) => {
-    currentImg = img;
+
+    await wait(2);
+
+    img.style.display = "none";
+
+    img = await createImage("img/img-2.jpg");
     console.log("Image 2 loaded");
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = "none";
-  })
-  .catch((err) => console.error(err));
+
+    await wait(2);
+    img.style.display = "none";
+  } catch (err) {
+    console.error(`${err}`);
+  }
+};
+
+// loadNPause();
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = await Promise.all(imgArr.map((img) => createImage(img)));
+
+    imgs.forEach((img) => img.classList.add("parallel"));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadAll(["img/img-1.jpg", "img/img-2.jpg", "img/img-3.jpg"]);
